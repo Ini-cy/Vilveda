d55d1345f32c17d7c52496ae76c93a609e461015
Inioluwa Olaleye (ido030) <ido030@usask.ca>
2022-11-17 15:11:30 -0600

    add javascript file

---
+++ vilveda.js
@@ -0,0 +1,26 @@
+// function change() {
+//    var words = ["TRADE","INVEST","SELL"],
+//    i = 0;
+//    setInterval(function(){ $('#words').fadeOut(function(){
+//        $(this).innerHTML(words[(i = (i + 1) % words.length)]).fadeIn();
+//      }); }, 1000)
+//  })
+//
+//  change();
+
+// document.getElementById("words").innerHTML = "yourTextHere";
+var i = 0;
+function change() {
+  var tag = document.getElementById("words");
+  var txt = ["INVEST","SELL","SAVE","TRADE"]
+  // tag.style.innerHTML = txt[i];
+  document.getElementById("words").innerHTML = txt[i];
+
+  i = (i + 1) % txt.length;
+
+}
+
+setInterval(change,2000);
+
+
+ // use a while loop instead
