a1725a82aaf32e711f6e0f52061e7b0e71189f31
Inioluwa Olaleye (ido030) <ido030@usask.ca>
2022-12-18 16:38:26 -0600

    refactor vilveda.js

--- vilveda.js
+++ vilveda.js
@@ -1,19 +1,7 @@
-// function change() {
-//    var words = ["TRADE","INVEST","SELL"],
-//    i = 0;
-//    setInterval(function(){ $('#words').fadeOut(function(){
-//        $(this).innerHTML(words[(i = (i + 1) % words.length)]).fadeIn();
-//      }); }, 1000)
-//  })
-//
-//  change();
-
-// document.getElementById("words").innerHTML = "yourTextHere";
 var i = 0;
 function change() {
   var tag = document.getElementById('words');
   var txt = ['INVEST', 'SELL', 'SAVE', 'TRADE'];
-  // tag.style.innerHTML = txt[i];
   document.getElementById('words').innerHTML = txt[i];
 
   i = (i + 1) % txt.length;
@@ -23,15 +11,17 @@ setInterval(change, 2000);
 
 var timer;
 var currentOpacity;
+// get the current opacity of the image and change it to 0.3
 var vilvedaImage = document.getElementById('vilveda-home-image-div');
 currentOpacity = getComputedStyle(vilvedaImage).getPropertyValue('opacity');
 currentOpacity = 0.3;
 
+// everytime the function is called it increments the opacity by 0.1
 function increaseOpacity() {
   currentOpacity += 0.1;
   vilvedaImage.style.opacity = currentOpacity;
 }
-
+// increment by 0.1 every 500 milliseconds (0.5 seconds)
 function runTime() {
   timer = setInterval(increaseOpacity, 500);
 }
@@ -40,12 +30,9 @@ if (currentOpacity < 1.0) {
   runTime();
 }
 
-
 window.addEventListener('scroll', function () {
   var value = window.scrollY;
 
   vilvedaImage.style.top = -value * 0.5 + 'px';
   console.log(vilvedaImage.style.top);
 });
-
-// use a while loop instead
